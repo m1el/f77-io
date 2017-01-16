daf21e07bbd9e78cea0464bc694eab90d677af2e
Igor null <m1el.2027@gmail.com>
2017-01-16 14:20:40 +0200

    added looping support for writer

--- src/write.rs
+++ src/write.rs
@@ -8,6 +8,9 @@ pub struct WriterOpts {
     suppress_newline: bool,
     scale: isize,
     radix: usize,
+    wants_data: bool,
+    consumed_data: bool,
+    has_something: bool,
 }
 
 #[cfg(never)]
@@ -18,6 +21,7 @@ pub struct FortranVmWriter<'a> {
 
 pub struct FortranIterWriter<'a> {
     iter: Peekable<FormatEvalIter<'a>>,
+    node: &'a FormatNode,
     opts: WriterOpts,
 }
 
@@ -33,31 +37,35 @@ impl<'a> FortranIterWriter<'a> {
                 suppress_newline: false,
                 scale: 0,
                 radix: 10,
+                consumed_data: false,
+                wants_data: true,
+                has_something: false,
             },
+            node: fmt,
             iter: fmt.into_iter().peekable(),
         }
     }
 
-    fn is_const(n: &FormatNode) -> Result<bool, ()> {
+    fn requires_data(n: &FormatNode) -> Result<bool, ()> {
         use format::FormatNode::*;
         let rv = match n {
-            &NewLine => true,
-            &SkipChar => true,
-            &SuppressNewLine => true,
-            &Terminate => true,
-            &BlankControl(_) => true,
-            &AbsColumn(_) => true,
-            &RelColumn(_) => true,
-            &Radix(_) => true,
-            &Scale(_) => true,
-            &Literal(_) => true,
+            &NewLine => false,
+            &SkipChar => false,
+            &SuppressNewLine => false,
+            &Terminate => false,
+            &BlankControl(_) => false,
+            &AbsColumn(_) => false,
+            &RelColumn(_) => false,
+            &Radix(_) => false,
+            &Scale(_) => false,
+            &Literal(_) => false,
 
-            &Str(_) => false,
-            &Bool(_) => false,
-            &Int(_, _) => false,
-            &Oct(_, _) => false,
-            &Hex(_, _) => false,
-            &Real(_, _, _, _) => false,
+            &Str(_) => true,
+            &Bool(_) => true,
+            &Int(_, _) => true,
+            &Oct(_, _) => true,
+            &Hex(_, _) => true,
+            &Real(_, _, _, _) => true,
             &Group(_) | &Repeat(_, _) => unreachable!(),
             &RemainingChars => return Err(()),
         };
@@ -69,15 +77,29 @@ impl<'a> FortranIterWriter<'a> {
     {
         use format::FormatNode::*;
         loop {
-            let cont = match self.iter.peek() {
-                Some(x) => try!(Self::is_const(*x)),
-                None => false,
-            };
-            if !cont {
-                break;
+            if self.iter.peek().is_none() {
+                if !self.opts.consumed_data && has_data {
+                    return Err(());
+                }
+                if !self.opts.has_something {
+                    return Ok(());
+                }
+                // we've reached the end of the pattern, reset the iterator
+                self.iter = self.node.into_iter().peekable();
             }
 
-            let next = self.iter.next().unwrap();
+            let next = match self.iter.next() {
+                None => break,
+                Some(x) => {
+                    self.opts.has_something = true;
+                    if try!(Self::requires_data(x)) {
+                        self.opts.wants_data = true;
+                        break;
+                    }
+                    x
+                }
+            };
+
             match next {
                 &Radix(r) => { self.opts.radix = r; },
                 &Scale(p) => { self.opts.scale = p; },
@@ -111,11 +133,28 @@ impl<'a> FortranIterWriter<'a> {
     pub fn write_value<W, T>(&mut self, dst: &mut W, val: &T) -> Result<(), ()>
         where W: Write, T: FortranFormat
     {
+        if !self.opts.wants_data {
+            return Err(());
+        }
         let n = match self.iter.next() {
             Some(n) => n,
             None => return Err(()),
         };
-        dst.write_all(val.f77_format(n, &self.opts).as_bytes())
-            .map_err(|_|())
+        self.opts.has_something = true;
+        self.opts.consumed_data = true;
+        self.opts.wants_data = false;
+        dst.write_all(val.f77_format(n, &self.opts).as_bytes()).map_err(|_|())
+    }
+
+    pub fn write_ary<W, T>(&mut self, dst: &mut W, ary: &[T]) -> Result<(), ()>
+        where W: Write, T: FortranFormat
+    {
+        for val in ary.iter() {
+            if !self.opts.wants_data {
+                try!(self.write_constants(dst, true).map_err(|_|()));
+            }
+            try!(self.write_value(dst, val).map_err(|_|()));
+        }
+        Ok(())
     }
 }
